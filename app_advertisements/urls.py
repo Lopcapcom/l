82c7cd23b575bb67a41b8a9b6457048761aec5ca
Lopcapcom <136692493+Lopcapcom@users.noreply.github.com>
2023-08-31 21:26:45 +0500

    Add files via upload

---
+++ app_advertisements/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path
+from .views import index, top_sellers, advertisement_post, advertisement_detail
+from  django.conf import settings
+
+
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='adv-post'),
+    path('advertisement/<int:pk>', advertisement_detail, name='adv-detail')
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
